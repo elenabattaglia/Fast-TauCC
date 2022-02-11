1d294aec21ab20901f352c6db9d8524b8d5abe56
elenabattaglia <53483400+elenabattaglia@users.noreply.github.com>
2022-02-11 13:36:03 +0100

    Add files via upload

---
+++ CoClust_3D_DBLP.py
@@ -0,0 +1,21 @@
+from algorithms.CreateTensor import readTensor
+from algorithms.tensor_coclust_incremental import CoClust
+from sklearn.metrics import normalized_mutual_info_score as nmi
+from sklearn.metrics import adjusted_rand_score as ari
+import numpy as np
+
+
+dataset = 'MovieLens1'   #DBLP, MovieLens1,MovieLens2, yelpTOR, yelpPGH
+
+
+data_path = './datasets/tensors/'
+T, target, label_mode = readTensor(dataset, data_path)
+    
+
+model = CoClust(verbose = True)
+model.fit(T)
+n = nmi(target, model.labels_[label_mode])
+a = ari(target, model.labels_[label_mode])
+
+print(f"nmi: {n}")
+print(f"ari: {a}")
